9ffe891bf71423a8bd04a4d80140b1da113991ad
keep_promise <1981672801@qq.com>
2023-10-15 23:35:42 +0800

    commit实现

--- src/react-dom/index.js
+++ src/react-dom/index.js
@@ -1,7 +1,8 @@
 // fiber解决同步渲染的问题，优化成异步渲染，链表【return、child、sibling】
 // 每一个element都是一个节点，转成链表结构
 // 结构：树 -》平铺结构
-// 异步渲染：类似 requestIdleCallback
+// 异步渲染：类似 requestIdleCallback -- render阶段
+// 同步 --- commit阶段
 
 // 基于fiber创建真实dom
 function createDOM(fiber) {
@@ -24,7 +25,7 @@ function createDOM(fiber) {
 // 
 function render(element, container) {
   // 初始化第一个fiber-->root fiber
-  nextUnitOfWork = {
+  wipRoot = {
     dom: container,
     props: {
       children: [element]
@@ -33,13 +34,31 @@ function render(element, container) {
     child: null,
     parent: null
   };
+  nextUnitOfWork = wipRoot;
 }
 
 // 调度渲染实现
 let nextUnitOfWork = null; // 下一次渲染的节点
+let wipRoot = null;
+
+// commit阶段 -- 这一个过程同步执行，不可中断
+function commitRoot() {
+  commitWork(wipRoot.child);
+  wipRoot = null;
+}
+
+function commitWork(fiber) {
+  if (!fiber) {
+    return;
+  }
+  const parentDOM = fiber.parent.dom;
+  parentDOM.append(fiber.dom);
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
+}
 
 // 调度函数
-function wookLoop(deadLine) {
+function workLoop(deadLine) {
   let shouldYield = false; // 是否中断
   // 有工作且不应该中断
   while(!shouldYield && nextUnitOfWork) {
@@ -47,7 +66,12 @@ function wookLoop(deadLine) {
     shouldYield = deadLine.timeRemaining() > 1; // 是否还有剩余时间
   }
   // 没有足够的时间，请求下一次浏览器空闲时间调度
-  requestIdleCallback(workLoop);
+  requestIdleCallback(workLoop); // 异步
+
+  // 没有fiber要render，且wipRoot存在，就执行commit阶段
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot()
+  }
 }
 
 // 第一次浏览器空闲时间调度
@@ -60,10 +84,13 @@ function performUnitOfWork(fiber) {
     fiber.dom = createDOM(fiber);
   }
 
-  // 追加父节点
-  if(fiber.parent) {
-    fiber.parent.dom.append(fiber.dom);
-  }
+
+  // 追加父节点 -- render阶段不要追加dom，render阶段和commit阶段分离
+  // 原因：[commit阶段]挂载到dom上，挂载部分dom时中断，出现部分数据更新，部分数据未更新
+  // 用户看到不完整，因此将挂载dom抽离到 [commit阶段]，同步挂载
+  // if(fiber.parent) {
+  //   fiber.parent.dom.append(fiber.dom);
+  // }
 
   // children创建fiber
   const children = fiber.props.children;
@@ -105,7 +132,6 @@ function performUnitOfWork(fiber) {
   }
 
 }
-
 
 export {
   render
