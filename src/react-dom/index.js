d75f3e281234ce89d8058ca6c86af34c98e71be1
keep_promise <1981672801@qq.com>
2023-10-15 23:10:08 +0800

    fiber结构创建

--- src/react-dom/index.js
+++ src/react-dom/index.js
@@ -1,19 +1,11 @@
-// 递归渲染的问题：同步渲染，递归树结构一旦开始，不能中断
-// Once we start rendering, we won’t stop until 
-// we have rendered the complete element tree. If 
-// the element tree is big, it may block the main 
-// thread for too long. And if the browser needs to 
-// do high priority stuff like handling user input 
-// or keeping an animation smooth, it will have to 
-// wait until the render finishes.
-
 // fiber解决同步渲染的问题，优化成异步渲染，链表【return、child、sibling】
 // 每一个element都是一个节点，转成链表结构
 // 结构：树 -》平铺结构
 // 异步渲染：类似 requestIdleCallback
 
-function render(element, container) {
-  const { type, props = {} } = element;
+// 基于fiber创建真实dom
+function createDOM(fiber) {
+  const { type, props = {} } = fiber;
   // 创建元素
   const dom = type === 'TEXT_ELEMENT' ? 
     document.createTextNode('') :
@@ -26,12 +18,21 @@ function render(element, container) {
     }
   });
 
-  // 递归渲染子元素
-  // 存在的问题：同步渲染，递归树结构一旦开始，不能中断
-  // props.children.forEach(child => render(child, dom));
+  return dom;
+}
 
-  // 渲染元素
-  container.appendChild(dom);
+// 
+function render(element, container) {
+  // 初始化第一个fiber-->root fiber
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element]
+    },
+    sibiling: null, 
+    child: null,
+    parent: null
+  };
 }
 
 // 调度渲染实现
@@ -52,8 +53,56 @@ function wookLoop(deadLine) {
 // 第一次浏览器空闲时间调度
 requestIdleCallback(workLoop);
 
-// 渲染节点，并返回下一个需要渲染的节点
-function performUnitOfWork(work) {
+// 渲染fiber节点，并返回下一个需要渲染的fiber节点
+function performUnitOfWork(fiber) {
+  // 创建dom元素
+  if(!fiber.dom) {
+    fiber.dom = createDOM(fiber);
+  }
+
+  // 追加父节点
+  if(fiber.parent) {
+    fiber.parent.dom.append(fiber.dom);
+  }
+
+  // children创建fiber
+  const children = fiber.props.children;
+  let preSibling = null;
+
+  // 构建fiber，并建立联系
+  for(let i = 0; i < children.length; i++) {
+    const child = children[i];
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      parent: fiber,
+      dom: null,
+      child: null,
+      sibling: null
+    };
+
+    // fiber.child指向第一个子元素
+    if (i==0) {
+      fiber.child = newFiber;
+    } else {
+      // 不是第一个就是兄弟
+      preSibling.sibling = newFiber;
+    }
+    // 保存上一个fiber，下一次循环，让他的兄弟指向基于其他儿子(非第一个儿子）创建的fiber
+    preSibling = newFiber;
+  }
+
+  // 返回下一个fiber
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while(nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibiling;
+    }
+    nextFiber = nextFiber.parent;
+  }
 
 }
 
